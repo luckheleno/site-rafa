e165c1ba32df8dcf690277eb590c197c6706898e
luckheleno <luckheleno@gmail.com>
2026-09-03 17:32:52 -0300

    Add files via upload

---
+++ zr-google-config.js
@@ -0,0 +1,2 @@
+/* Cole aqui a URL /exec do Google Apps Script depois de publicá-lo. */
+window.ZR_GOOGLE_API = 'https://script.google.com/macros/s/AKfycbxzdVP3PeOjXWvH9tGXInxcTgRNngQOnJgqvGrfZ-Hs_qO2RBD0e4ASsJih5KXEbtf-/exec';
